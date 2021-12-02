17a1d8302825a370fe7373e33e144e746512b32e
Mike LeBlanc <86264329+MoistButteryBiscuits@users.noreply.github.com>
2021-12-02 00:54:07 +0000

    Add my own information

--- follow.js
+++ follow.js
@@ -49,15 +49,12 @@
 */
 
 // Edit below this line =============
-const birthYear = 1948;
-const thisYear = 1965;
-const firstName = "Carlos";
-const lastName = "Stevenson";
+const firstName = "Micheal";
+const lastName = "LeBlanc";
+const thisYear = 2021;
+const birthYear = 1992;
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
-
-console.log(greeting);
-
+const greeting = "My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old."
 
 
 
